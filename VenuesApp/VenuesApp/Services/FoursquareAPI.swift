942a95941b9a972047f144fe14f7e7e45502b0f9
GEORGE QUENTIN <ggeoo93@gmail.com>
2019-05-19 22:05:40 +0100

    added fetchCategories functionality with default placeholder to fetch categories from Foursquare API

--- VenuesApp/VenuesApp/Services/FoursquareAPI.swift
+++ VenuesApp/VenuesApp/Services/FoursquareAPI.swift
@@ -28,13 +28,16 @@ final class FoursquareAPI {
             .addObserver(self, selector: #selector(downloadTransactionData(with:)), name: .downloadImageNotification, object: nil)
     }
     
-    private var placeHolderVenue: [String: Any] {
-        return ["id":"", "name":"", "categories": Category(name: "", iconUrl: ""),
-                "country":"", "latitude":0.0, "longitude":0.0] as [String : Any]
+    private var venuePlaceholder: [String: Any] {
+        return ["id":"", "name":"", "address":"",
+                "country":"", "distance":0.0, "latitude":0.0, "longitude":0.0] as [String : Any]
+    }
+    
+    private var categoryPlaceholder: [String: Any] {
+        return ["id":"", "name":"", "shortName": "", "country":""] as [String : Any]
     }
     // MARK: - mock Venues data
     func mockVenueData() -> [Venue] {
-        // Venue(data: "id":"", "name":"", "category":"", "country":"", "iconUrl":"", "description":"", "latitude":0.0, "longitude":0.0)
         return [
             Venue(data: [
                 "id":"4de50c65fa7651589f3c2cf3",
@@ -45,27 +48,27 @@ final class FoursquareAPI {
     }
     
     // MARK: - Networking to fetch Venues data
-    private enum SearchType {
-        case venuesInArea, venuesOfInterest
+    func openWeb(of venueId: String) {
+        if let encodedAddress =
+            ("https://www.foursquare.com/v/"+venueId).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+            let url = URL(string: encodedAddress) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
     }
-    func fetchVenues(with spanRadius: Double, at location: CLLocationCoordinate2D, completion: @escaping ([Venue]) -> () ) {
-        let urlString =
-        "https://api.foursquare.com/v2/venues/search" +
-        "?client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=\(VERSION)&limit=\(50)" +
-        "&ll=\(location.latitude),\(location.longitude)&radius=\(spanRadius)&intent=checkin"
-        fetchVenues(with: urlString, type: .venuesInArea, completion: completion)
-    }
-    
     func fetchVenues(using query: String, with spanRadius: Double, at location: CLLocationCoordinate2D, completion: @escaping ([Venue]) -> () ) {
+        let section = query.lowercased().replacingOccurrences(of: " ", with: "")
         let urlString =
-        "https://api.foursquare.com/v2/venues/search" +
-        "?client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=\(VERSION)&limit=\(10)" +
-        "&ll=\(location.latitude),\(location.longitude)&radius=\(spanRadius)&checkin=browse&query=\(query)"
-        fetchVenues(with: urlString, type: .venuesOfInterest, completion: completion)
+        "https://api.foursquare.com/v2/venues/explore" +
+        "?client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=\(VERSION)" +
+        "&limit=\(20)&ll=\(location.latitude),\(location.longitude)&radius=\(spanRadius)&section=\(section)"
+        fetchVenues(with: urlString, completion: completion)
     }
-    
-    private func fetchVenues(with urlString: String, type: SearchType, completion: @escaping ([Venue]) -> ()) {
-        guard let url = URL(string: urlString) else { return }
+    private func fetchVenues(with urlString: String, completion: @escaping ([Venue]) -> ()) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
         URLSession.shared.dataTask(with: url) { [weak self] (dataresult, response, error) in
             guard let this = self else {
                 completion([])
@@ -87,17 +90,30 @@ final class FoursquareAPI {
                 guard
                     // 3
                     let dictionary = json as? [String: Any],
-                    
                     // 4
                     let venuesResponse = dictionary["response"] as? [String: Any]
                     else {
                         fatalError("JSON decoding error of venues locations.")
                 }
-                
+                //print(venuesResponse)
                 var venues = [Venue]()
-                switch type {
-                case .venuesInArea: this.getVenuesOfArea(with: &venues, response: venuesResponse)
-                case .venuesOfInterest: this.getVenuesOfArea(with: &venues, response: venuesResponse)
+                for groups in venuesResponse["groups"] as! [[String: AnyObject]] {
+                    for items in groups["items"] as! [[String: AnyObject]] {
+                        if let venue = items["venue"] as? [String: AnyObject] {
+                            var venueData = this.venuePlaceholder
+                            venueData["id"] = venue["id"] as! String
+                            venueData["name"] = venue["name"] as! String
+                            if let location = venue["location"] as? [String: AnyObject],
+                                let formattedAddress = location["formattedAddress"] as? [String] {
+                                venueData["address"] = formattedAddress.joined(separator: "\n")
+                                venueData["country"] = location["country"] as! String
+                                venueData["distance"] = location["distance"] as! Double
+                                venueData["latitude"] = location["lat"] as! Double
+                                venueData["longitude"] = location["lng"] as! Double
+                            }
+                            venues.append(Venue(data: venueData))
+                        }
+                    }
                 }
                 this.persistencyManager.saveVenuesData(with: data)
                 
@@ -109,34 +125,68 @@ final class FoursquareAPI {
             }
         }.resume()
     }
-    
-    private func getVenuesOfArea(with venues: inout [Venue], response: [String: Any]) {
-        for dict in response["venues"] as! [[String: AnyObject]] {
-            var data = placeHolderVenue
-            data["id"] = dict["id"] as! String
-            data["name"] = dict["name"] as! String
-            var categories = [Category]()
-            for catergory in dict["categories"] as! [[String: AnyObject]] {
-                let categoryName = catergory["name"] as! String
-                if let icon = catergory["icon"] as? [String: AnyObject] {
-                    let prefix = icon["prefix"] as! String
-                    let suffix = icon["suffix"] as! String
-                    categories.append(Category(name: categoryName, iconUrl: prefix + suffix))
+    func fetchVenueCategories(completion: @escaping ([Category]) -> () ) {
+        let urlString =
+        "https://api.foursquare.com/v2/venues/categories" +
+        "?client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=\(VERSION)"
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] (dataresult, response, error) in
+            guard let this = self else {
+                completion([])
+                return
+            }
+            if error != nil {
+                fatalError("Error getting categories from Foursquare: \(String(describing: error)).")
+            }
+            do {
+                
+                guard
+                    // 1
+                    let data = dataresult else {
+                        fatalError("Data result of categories corrupted = nil.")
                 }
+                
+                // 2
+                let json = try JSONSerialization.jsonObject(with: data)
+                guard
+                    // 3
+                    let dictionary = json as? [String: Any],
+                    // 4
+                    let categoriesResponse = dictionary["response"] as? [String: Any]
+                    else {
+                        fatalError("JSON decoding error of categories.")
+                }
+                //print(categoriesResponse)
+               
+                var categories = [Category]()
+                this.fetchNestedCategories(with: &categories, response: categoriesResponse)
+                this.persistencyManager.saveVenuesData(with: data)
+                let uniqueCategories = Array(Set<Category>(categories))
+                DispatchQueue.main.async(execute: { () -> Void in
+                    completion(uniqueCategories)
+                })
+            } catch let err {
+                fatalError("Could not fetch Categories Data with URL: \(err)")
             }
-            data["categories"] = categories
-            if let location = dict["location"] as? [String: AnyObject] {
-                data["country"] = location["country"] as! String
-                data["latitude"] = location["lat"] as! Double
-                data["longitude"] = location["lng"] as! Double
-            }
-            venues.append(Venue(data: data))
-        }
+        }.resume()
     }
     
-    private func getVenuesOfInterest(with venues: inout [Venue], response: [String: Any]) {
-        for dict in response["venues"] as! [[String: AnyObject]] {
-            
+    func fetchNestedCategories(with categories: inout [Category], response: [String: Any]) {
+        if let mainCategories = response["categories"] as? [[String: AnyObject]] {
+            for dict in mainCategories {
+                fetchNestedCategories(with: &categories, response: dict)
+                if let icon = dict["icon"] as? [String: AnyObject] {
+                    var categoryData = categoryPlaceholder
+                    categoryData["id"] = dict["id"] as! String
+                    categoryData["name"] = dict["name"] as! String
+                    categoryData["shortName"] = dict["shortName"] as! String
+                    let prefix = icon["prefix"] as! String
+                    let suffix = icon["suffix"] as! String
+                    categoryData["iconUrl"] = prefix + suffix
+                    
+                    categories.append(Category(data:categoryData))
+                }
+            }
         }
     }
     
@@ -148,7 +198,6 @@ final class FoursquareAPI {
             let filename = URL(string: imageUrl)?.lastPathComponent else {
                 return
         }
-        print(imageUrl)
         if let savedImage = persistencyManager.getImage(with: filename) {
             imageView.image = savedImage
             return
